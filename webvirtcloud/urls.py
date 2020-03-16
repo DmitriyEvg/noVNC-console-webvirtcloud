3b0854db2851405cc01606789960fec2435736b5
root <root@bitnet.pro>
2020-03-16 19:28:55 +0300

    update URL's for new ver. django

--- webvirtcloud/urls.py
+++ webvirtcloud/urls.py
@@ -1,33 +1,19 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+
+from instances.views import index
+from console.views import console
 # from django.contrib import admin
 
-urlpatterns = patterns('',
-    url(r'^$', 'instances.views.index', name='index'),
-    url(r'^instances/$', 'instances.views.instances', name='instances'),
+urlpatterns = [
+    url(r'^$', index, name='index'),
 
-    url(r'^instance/', include('instances.urls')),
+    url(r'^instances/', include('instances.urls')),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^computes/', include('computes.urls')),
     url(r'^logs/', include('logs.urls')),
-
-    url(r'^compute/(?P<compute_id>[0-9]+)/storages/$',
-        'storages.views.storages', name='storages'),
-    url(r'^compute/(?P<compute_id>[0-9]+)/storage/(?P<pool>[\w\-\.\/]+)/$',
-        'storages.views.storage', name='storage'),
-    url(r'^compute/(?P<compute_id>[0-9]+)/networks/$',
-        'networks.views.networks', name='networks'),
-    url(r'^compute/(?P<compute_id>[0-9]+)/network/(?P<pool>[\w\-\.]+)/$',
-        'networks.views.network', name='network'),
-    url(r'^compute/(?P<compute_id>[0-9]+)/interfaces/$',
-        'interfaces.views.interfaces', name='interfaces'),
-    url(r'^compute/(?P<compute_id>[0-9]+)/interface/(?P<iface>[\w\-\.\:]+)/$',
-        'interfaces.views.interface', name='interface'),
-    url(r'^compute/(?P<compute_id>[0-9]+)/secrets/$',
-        'secrets.views.secrets', name='secrets'),
-    url(r'^compute/(?P<compute_id>[0-9]+)/create/$',
-        'create.views.create_instance', name='create_instance'),
-
-    url(r'^console/$', 'console.views.console', name='console'),
-    url(r'^console/actions/', 'console.views.VM_actions', name='VM_actions'),
-    # (r'^admin/', include(admin.site.urls)),
-)
+    url(r'^datasource/', include('datasource.urls')),
+    #url(r'^console/$', console, name='console'),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^console/$', console, name='console'),
+    url(r'^console/actions/', console, name='VM_actions'),
+]
